77c66a41c58d3f3b71f59997a76ccefb5529c9cd
Ladoxer <ladderdoxer@gmail.com>
2023-10-19 13:38:40 +0530

    1st assignment completed

--- src/User.repository.ts
+++ src/User.repository.ts
@@ -1,4 +1,10 @@
 import Database, { UserRequestDto } from './Database'
 
 // NOTE: Make db public instead of private. This is required for the tests.
-export class UserRepository {}
+export class UserRepository {
+  public readonly db = Database;
+
+  async createUser(userdata: UserRequestDto) {
+    return this.db.create(userdata);
+  }
+}
--- src/User.service.ts
+++ src/User.service.ts
@@ -2,4 +2,10 @@ import { UserRequestDto } from './Database'
 import { UserRepository } from './User.repository'
 
 // NOTE: Make userRepo public and not private. This is required for the tests!
-export class UserService {}
+export class UserService {
+  constructor(public userRepo: UserRepository){}
+
+  async createUser(userdata: UserRequestDto) {
+    return this.userRepo.createUser(userdata);
+  }
+}
